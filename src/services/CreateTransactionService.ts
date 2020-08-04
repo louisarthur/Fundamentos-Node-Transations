ea9ccf7eacf86a417d0752bd7eb8402ab67cb64f
Louis Arthur <louisarthur@MacBook-Pro-de-Louis.local>
2020-08-04 13:21:22 -0300

    adding new features to submit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,20 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && total < value) {
+      throw new Error('The value is more than we have inbound');
+    }
+    if (value < 0) {
+      throw Error('The value is negative, values negative can not be addicted');
+    }
+    const transaction = this.transactionsRepository.createTransaction({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
